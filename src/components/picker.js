316cd1272ad3a1d5363efacb8c4d6fc9a353ce4e
EO <eo.code.wd@gmail.com>
2024-03-22 15:16:33 +0100

    picker update

--- src/components/picker.js
+++ src/components/picker.js
@@ -1,20 +1,21 @@
-import {Picker} from '@react-native-picker/picker';
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 
-import React,{useState} from 'react'
+export default function MyPicker() {
+  // Initialize the item state before using it
+  const [item, setItem] = useState('');
 
-export default function picker() {
-    setItem(itemValue)
-    const [item,setItem]=useState('')
   return (
-    <div>
+    <View>
       <Picker
-  selectedValue={selectedLanguage}
-  onValueChange={(itemValue, itemIndex) =>
-    setItem(itemValue)
-  }>
-  <Picker.Item label="Java" value="java" />
-  <Picker.Item label="JavaScript" value="js" />
-</Picker>
-    </div>
-  )
+        selectedValue={item} // Use the state variable here
+        onValueChange={(itemValue, itemIndex) =>
+          setItem(itemValue)
+        }>
+        <Picker.Item label="Java" value="java" />
+        <Picker.Item label="JavaScript" value="js" />
+      </Picker>
+    </View>
+  );
 }
